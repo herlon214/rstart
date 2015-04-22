9ed34cfa6a22fef5bd97ede0f140ff9d354239f6
Herlon Aguiar <herlon214@gmail.com>
2015-04-22 17:38:13 -0400

    [FIX] All the 2xx response code are "OK"

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ rstart.prototype.check = function(callback) {
 
   var response = function(error, response, body) {
 
-    if (!error && response.statusCode == 200) {
+    if (!error && (response.statusCode >= 200 && response.statusCode <= 300)) {
       w.info('Response OK > '+self.url);
       self.emit('response', 'OK');
     }else {
